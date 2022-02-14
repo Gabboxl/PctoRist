62065aad6b3e4ccc8508d05813ed40d743b9f0d7
Poliziescu <stoicescueric05@gmail.com>
2022-02-14 12:45:39 +0100

    iggyg

--- app/src/main/java/it/itispininfarina/pctorist/OrdiniFragment.kt
+++ app/src/main/java/it/itispininfarina/pctorist/OrdiniFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
-class OrdiniFragment : Fragment() {
+class OrdiniFragment : Fragment(), AdapterView.OnItemSelectedListener  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,35 @@ class OrdiniFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ordini, container, false)
+        val layout = inflater.inflate(R.layout.fragment_ordini, container, false)
+
+        //adattamento dello spinner
+
+        val spinner: Spinner= layout.findViewById(R.id.spinnerMarinara)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.quantità,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinner.adapter = adapter
+
+        }
+        //FINE ADATTAMENTO SPINNER
+
+
+        return layout
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        val quantity = parent?.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
 
