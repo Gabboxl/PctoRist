44a6eca5b06ac8d843412b706839b107e61292e1
Gabriele <gabboxl0@gmail.com>
2022-02-14 11:15:13 +0100

    nuova roba

--- app/src/main/java/it/itispininfarina/pctorist/MainActivity.kt
+++ app/src/main/java/it/itispininfarina/pctorist/MainActivity.kt
@@ -1,17 +1,15 @@
 package it.itispininfarina.pctorist
 
 import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
