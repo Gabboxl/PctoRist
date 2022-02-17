9c5308ca04d96b9b51319a31c4bc02dffe2da177
Gabriele <gabboxl0@gmail.com>
2022-02-17 11:55:59 +0100

    rinominati alcuni file pt.2

--- app/src/main/java/it/itispininfarina/pctorist/InfoFragment.kt
+++ app/src/main/java/it/itispininfarina/pctorist/InfoFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class InforistFragment : Fragment() {
+class InfoFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
